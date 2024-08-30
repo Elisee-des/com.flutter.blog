eca41ac7a45ac7dc17ad3d629606a8717048315b
Elisee-des <esabidani@gmail.com>
2024-08-30 22:43:11 +0000

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:blog/core/theme/theme.dart';
+import 'package:blog/features/auth/presentation/pages/login_page.dart';
+import 'package:blog/features/auth/presentation/pages/sign_up_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Blog App',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.darkThemeMode,
+      home: const LoginPage(),
+    );
+  }
+}
